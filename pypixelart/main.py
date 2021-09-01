37ce184fb9468f7232cdd0e34e426b386f06eedf
Douglas Carvalho <douglasc.dev@gmail.com>
2021-09-01 19:23:08 -0300

    Refactored core initializations and loop into a class

--- pypixelart/main.py
+++ pypixelart/main.py
@@ -1,4 +1,5 @@
 import itertools
+import pathlib
 import sys
 from enum import IntEnum
 from pathlib import Path
@@ -331,6 +332,227 @@ def print_welcome_msg(func):
     return wrapper
 
 
+class PyPixelArt:
+    def __init__(self, image: pg.Surface, path: pathlib.Path):
+        self.image, self.path = image, path
+
+        self.screen = pg.display.set_mode((600, 600), pg.RESIZABLE)
+        self.app_name = click.get_current_context().command.name
+        pg.display.set_caption(self.app_name)
+
+        self.cursor_rect = pg.Rect((0, 0), (0, 0))
+        self.cursor_draw_color = {"color": white}
+
+        self.resized_img_rect = self.last_resized_img_rect = None
+
+        self.resized_img = self.rectangle_rect = None
+
+        self.line_width = 4
+        self.cursor_line_width = self.line_width // 2
+        self.grid_line_width = 1
+        self.symmetry_line_width = 4
+
+        self.clock = pg.time.Clock()
+
+        self.zoom = {
+            "percent": 1000,
+            "step": 100,
+            "changed": False,
+        }
+
+        self.grid = {
+            "on": False,
+        }
+
+        self.color_selection = {
+            "on": False,
+        }
+
+        self.show_bindings = {
+            "on": False,
+        }
+
+        self.help_keybinding = KeyBinding(pg.K_SPACE, "Help", lambda: self.set_show_bindings())
+
+        self.palette_colors = {
+            "red": pg.Color(172, 50, 50),
+            "cream": pg.Color(217, 160, 102),
+            "brown": pg.Color(102, 57, 49),
+            "black": pg.Color(0, 0, 0),
+            "blue": pg.Color(91, 110, 225),
+            "yellow": pg.Color(251, 242, 54),
+            "alpha": pg.Color(0, 0, 0, 0),
+        }
+
+        self.image_history = list()
+
+        self.symmetry = {"status": SymmetryType.NoSymmetry}
+
+        zoom_g, cursor_g = "Zoom", "Move Cursor"
+        self.keybindings = [
+            KeyBinding(pg.K_i, "Draw", lambda: self.draw_pixel(), on_pressed=True),
+            KeyBinding(pg.K_u, "Undo", lambda: self.undo(), on_pressed=True),
+            KeyBinding(pg.K_w, "Save file", lambda: self.save()),
+            KeyBinding(pg.K_n, zoom_g, lambda: self.change_zoom(True), on_pressed=True),
+            KeyBinding(pg.K_b, zoom_g, lambda: self.change_zoom(False), on_pressed=True),
+            KeyBinding(pg.K_k, cursor_g, lambda: self.move_cursor(0, -1)),
+            KeyBinding(pg.K_j, cursor_g, lambda: self.move_cursor(0, 1)),
+            KeyBinding(pg.K_l, cursor_g, lambda: self.move_cursor(1, 0)),
+            KeyBinding(pg.K_h, cursor_g, lambda: self.move_cursor(-1, 0)),
+            KeyBinding(pg.K_g, "Grid", lambda: self.set_grid()),
+            KeyBinding(pg.K_s, "Symmetry", lambda: self.set_symmetry()),
+            KeyBinding(pg.K_ESCAPE, "Exit", lambda: sys.exit()),
+            KeyBinding(pg.K_c, "Color selection", lambda: self.set_color_selection()),
+        ]
+
+        self.keybindings += [
+            KeyBinding(
+                pg.key.key_code(str(i)),
+                "Color",
+                lambda c=color: self.set_cursor_color(c),
+            )
+            for i, (name, color) in enumerate(self.palette_colors.items(), start=1)
+        ]
+
+        self.keybindings += [self.help_keybinding]
+
+    def change_zoom(self, is_positive: bool):
+        to_add = self.zoom["step"] if is_positive else -self.zoom["step"]
+        if self.zoom["percent"] + to_add > 0:
+            self.zoom["changed"] = True
+            self.zoom["percent"] += to_add
+
+    def move_cursor(self, x: int, y: int):
+        self.cursor_rect.move_ip(x * self.cursor_rect.w, y * self.cursor_rect.h)
+
+    def set_grid(self):
+        self.grid["on"] = not self.grid["on"]
+
+    def set_symmetry(self):
+        self.symmetry["status"] = SymmetryType(
+            (self.symmetry["status"].value + 1) % len(list(SymmetryType))
+        )
+
+    def set_color_selection(self):
+        self.color_selection["on"] = not self.color_selection["on"]
+
+    def set_show_bindings(self):
+        self.show_bindings["on"] = not self.show_bindings["on"]
+
+    def set_cursor_color(self, selected_color: pg.Color):
+        self.color_selection["on"] = False
+        self.cursor_draw_color["color"] = selected_color
+
+    def cursor_coords_in_pixels(self) -> Tuple[int, int]:
+        if not all((self.cursor_rect, self.last_resized_img_rect)):
+            return 0, 0
+        coord_x = (self.cursor_rect.x - self.last_resized_img_rect.x) // self.cursor_rect.w
+        coord_y = (self.cursor_rect.y - self.last_resized_img_rect.y) // self.cursor_rect.h
+        return coord_x, coord_y
+
+    def draw_pixel(self):
+        cursor_x, cursor_y = self.cursor_coords_in_pixels()
+        self.image_history.append(self.image.copy())
+        self.image.set_at((cursor_x, cursor_y), self.cursor_draw_color["color"])
+
+        if self.symmetry["status"] == SymmetryType.NoSymmetry:
+            return
+
+        middle_w, middle_h = self.image.get_width() // 2, self.image.get_height() // 2
+        if self.symmetry["status"] == SymmetryType.Vertical:
+
+            cursor_x = middle_w + (middle_w - cursor_x) - 1
+            self.image.set_at((cursor_x, cursor_y), self.cursor_draw_color["color"])
+        elif self.symmetry["status"] == SymmetryType.Horizontal:
+
+            cursor_y = middle_h + (middle_h - cursor_y) - 1
+            self.image.set_at((cursor_x, cursor_y), self.cursor_draw_color["color"])
+
+    def undo(self):
+        if self.image_history:
+            saved_img: pg.Surface = self.image_history.pop()
+            self.image.fill((0, 0, 0, 0))
+            self.image.blit(saved_img, (0, 0), saved_img.get_rect())
+
+    def save(self):
+        pg.image.save(self.image, self.path)
+        click.echo(f"Saved {self.path}")
+
+    def run_loop(self):
+
+        while True:
+            self.screen.fill(grey)
+
+            draw_header_text(
+                app_name=self.app_name,
+                path_name=self.path.name,
+                width=self.image.get_width(),
+                height=self.image.get_height(),
+                zoom=self.zoom["percent"],
+            )
+
+            self.last_resized_img_rect = self.resized_img_rect
+            self.resized_img, self.resized_img_rect = draw_resized_image(self.image, self.zoom["percent"])
+
+            self.rectangle_rect = draw_rect_around_resized_img(
+                self.resized_img, self.resized_img_rect, self.line_width
+            )
+
+            update_cursor_pos(
+                resized_img_rect=self.resized_img_rect,
+                last_resized_img_rect=self.last_resized_img_rect,
+                original_image_rect=self.image.get_rect(),
+                cursor_rect=self.cursor_rect,
+                zoom=self.zoom,
+                cursor_coords=self.cursor_coords_in_pixels(),
+            )
+
+            if self.grid["on"]:
+                where = self.resized_img.get_rect().move(
+                    (self.resized_img_rect.x, self.resized_img_rect.y)
+                )
+                grid_rect_size = self.cursor_rect.w, self.cursor_rect.h
+                draw_grid(where, grid_rect_size, self.grid_line_width)
+
+            draw_symmetry_line(
+                self.symmetry["status"],
+                self.resized_img.get_rect().move((self.resized_img_rect.x, self.resized_img_rect.y)),
+                self.symmetry_line_width,
+            )
+
+            cursor_image_color = black if self.grid["on"] else white
+            pg.draw.rect(self.screen, cursor_image_color, self.cursor_rect, width=self.cursor_line_width)
+
+            cursor_coords_text_rect = draw_cursor_coordinates(self.cursor_coords_in_pixels(),
+                                                              self.rectangle_rect.topleft)
+
+            rect_top_right_corner_x, _ = self.rectangle_rect.topright
+            draw_selected_color(
+                self.cursor_draw_color["color"],
+                rect_top_right_corner_x=rect_top_right_corner_x,
+                cursor_coord_text_y=cursor_coords_text_rect.y,
+            )
+
+            previous_cursor_x, previous_cursor_y = self.cursor_rect.x, self.cursor_rect.y
+
+            handle_input(self.keybindings)
+
+            if not self.resized_img_rect.colliderect(self.cursor_rect):
+                self.cursor_rect.x, self.cursor_rect.y = previous_cursor_x, previous_cursor_y
+
+            if self.show_bindings["on"]:
+                draw_keybindings(self.keybindings, self.line_width)
+            else:
+                draw_help_keybind(self.help_keybinding, self.rectangle_rect)
+
+            if self.color_selection["on"]:
+                draw_color_selection(self.palette_colors, self.line_width)
+
+            pg.display.flip()
+
+            self.clock.tick(60)
+
+
 @print_welcome_msg
 @click.command(name="pypixelart")
 @click.option(
@@ -359,216 +581,8 @@ def main(filepath, resolution):
         )
         image = pg.Surface(img_size, pygame.SRCALPHA)
 
-    screen = pg.display.set_mode((600, 600), pg.RESIZABLE)
-    app_name = click.get_current_context().command.name
-    pg.display.set_caption(app_name)
-
-    cursor_rect = pg.Rect((0, 0), (0, 0))
-    cursor_draw_color = {"color": white}
-
-    resized_img_rect = last_resized_img_rect = None
-
-    line_width = 4
-    cursor_line_width = line_width // 2
-    grid_line_width = 1
-    symmetry_line_width = 4
-
-    clock = pg.time.Clock()
-
-    zoom = {
-        "percent": 1000,
-        "step": 100,
-        "changed": False,
-    }
-
-    grid = {
-        "on": False,
-    }
-
-    color_selection = {
-        "on": False,
-    }
-
-    show_bindings = {
-        "on": False,
-    }
-
-    help_keybinding = KeyBinding(pg.K_SPACE, "Help", lambda: set_show_bindings())
-
-    palette_colors = {
-        "red": pg.Color(172, 50, 50),
-        "cream": pg.Color(217, 160, 102),
-        "brown": pg.Color(102, 57, 49),
-        "black": pg.Color(0, 0, 0),
-        "blue": pg.Color(91, 110, 225),
-        "yellow": pg.Color(251, 242, 54),
-        "alpha": pg.Color(0, 0, 0, 0),
-    }
-
-    image_history = list()
-
-    symmetry = {"status": SymmetryType.NoSymmetry}
-
-    def change_zoom(is_positive: bool):
-        to_add = zoom["step"] if is_positive else -zoom["step"]
-        if zoom["percent"] + to_add > 0:
-            zoom["changed"] = True
-            zoom["percent"] += to_add
-
-    def move_cursor(x: int, y: int):
-        cursor_rect.move_ip(x * cursor_rect.w, y * cursor_rect.h)
-
-    def set_grid():
-        grid["on"] = not grid["on"]
-
-    def set_symmetry():
-        symmetry["status"] = SymmetryType(
-            (symmetry["status"].value + 1) % len(list(SymmetryType))
-        )
-
-    def set_color_selection():
-        color_selection["on"] = not color_selection["on"]
-
-    def set_show_bindings():
-        show_bindings["on"] = not show_bindings["on"]
-
-    def set_cursor_color(selected_color: pg.Color):
-        color_selection["on"] = False
-        cursor_draw_color["color"] = selected_color
-
-    def cursor_coords_in_pixels() -> Tuple[int, int]:
-        if not all((cursor_rect, last_resized_img_rect)):
-            return 0, 0
-        coord_x = (cursor_rect.x - last_resized_img_rect.x) // cursor_rect.w
-        coord_y = (cursor_rect.y - last_resized_img_rect.y) // cursor_rect.h
-        return coord_x, coord_y
-
-    def draw_pixel():
-        cursor_x, cursor_y = cursor_coords_in_pixels()
-        image_history.append(image.copy())
-        image.set_at((cursor_x, cursor_y), cursor_draw_color["color"])
-
-        if symmetry["status"] == SymmetryType.NoSymmetry:
-            return
-
-        middle_w, middle_h = image.get_width() // 2, image.get_height() // 2
-        if symmetry["status"] == SymmetryType.Vertical:
-
-            cursor_x = middle_w + (middle_w - cursor_x) - 1
-            image.set_at((cursor_x, cursor_y), cursor_draw_color["color"])
-        elif symmetry["status"] == SymmetryType.Horizontal:
-
-            cursor_y = middle_h + (middle_h - cursor_y) - 1
-            image.set_at((cursor_x, cursor_y), cursor_draw_color["color"])
-
-    def undo():
-        if image_history:
-            saved_img: pg.Surface = image_history.pop()
-            image.fill((0, 0, 0, 0))
-            image.blit(saved_img, (0, 0), saved_img.get_rect())
-
-    def save():
-        pg.image.save(image, filepath)
-        click.echo(f"Saved {filepath}")
-
-    zoom_g, cursor_g = "Zoom", "Move Cursor"
-    keybindings = [
-        KeyBinding(pg.K_i, "Draw", lambda: draw_pixel(), on_pressed=True),
-        KeyBinding(pg.K_u, "Undo", lambda: undo(), on_pressed=True),
-        KeyBinding(pg.K_w, "Save file", lambda: save()),
-        KeyBinding(pg.K_n, zoom_g, lambda: change_zoom(True), on_pressed=True),
-        KeyBinding(pg.K_b, zoom_g, lambda: change_zoom(False), on_pressed=True),
-        KeyBinding(pg.K_k, cursor_g, lambda: move_cursor(0, -1)),
-        KeyBinding(pg.K_j, cursor_g, lambda: move_cursor(0, 1)),
-        KeyBinding(pg.K_l, cursor_g, lambda: move_cursor(1, 0)),
-        KeyBinding(pg.K_h, cursor_g, lambda: move_cursor(-1, 0)),
-        KeyBinding(pg.K_g, "Grid", lambda: set_grid()),
-        KeyBinding(pg.K_s, "Symmetry", lambda: set_symmetry()),
-        KeyBinding(pg.K_ESCAPE, "Exit", lambda: sys.exit()),
-        KeyBinding(pg.K_c, "Color selection", lambda: set_color_selection()),
-    ]
-
-    keybindings += [
-        KeyBinding(
-            pg.key.key_code(str(i)),
-            "Color",
-            lambda c=color: set_cursor_color(c),
-        )
-        for i, (name, color) in enumerate(palette_colors.items(), start=1)
-    ]
-
-    keybindings += [help_keybinding]
-
-    while True:
-        screen.fill(grey)
-
-        draw_header_text(
-            app_name=app_name,
-            path_name=path.name,
-            width=image.get_width(),
-            height=image.get_height(),
-            zoom=zoom["percent"],
-        )
-
-        last_resized_img_rect = resized_img_rect
-        resized_img, resized_img_rect = draw_resized_image(image, zoom["percent"])
-
-        rectangle_rect = draw_rect_around_resized_img(
-            resized_img, resized_img_rect, line_width
-        )
-
-        update_cursor_pos(
-            resized_img_rect=resized_img_rect,
-            last_resized_img_rect=last_resized_img_rect,
-            original_image_rect=image.get_rect(),
-            cursor_rect=cursor_rect,
-            zoom=zoom,
-            cursor_coords=cursor_coords_in_pixels(),
-        )
-
-        if grid["on"]:
-            where = resized_img.get_rect().move(
-                (resized_img_rect.x, resized_img_rect.y)
-            )
-            grid_rect_size = cursor_rect.w, cursor_rect.h
-            draw_grid(where, grid_rect_size, grid_line_width)
-
-        draw_symmetry_line(
-            symmetry["status"],
-            resized_img.get_rect().move((resized_img_rect.x, resized_img_rect.y)),
-            symmetry_line_width,
-        )
-
-        cursor_image_color = black if grid["on"] else white
-        pg.draw.rect(screen, cursor_image_color, cursor_rect, width=cursor_line_width)
-
-        cursor_coords_text_rect = draw_cursor_coordinates(cursor_coords_in_pixels(), rectangle_rect.topleft)
-
-        rect_top_right_corner_x, _ = rectangle_rect.topright
-        draw_selected_color(
-            cursor_draw_color["color"],
-            rect_top_right_corner_x=rect_top_right_corner_x,
-            cursor_coord_text_y=cursor_coords_text_rect.y,
-        )
-
-        previous_cursor_x, previous_cursor_y = cursor_rect.x, cursor_rect.y
-
-        handle_input(keybindings)
-
-        if not resized_img_rect.colliderect(cursor_rect):
-            cursor_rect.x, cursor_rect.y = previous_cursor_x, previous_cursor_y
-
-        if show_bindings["on"]:
-            draw_keybindings(keybindings, line_width)
-        else:
-            draw_help_keybind(help_keybinding, rectangle_rect)
-
-        if color_selection["on"]:
-            draw_color_selection(palette_colors, line_width)
-
-        pg.display.flip()
-
-        clock.tick(60)
+    pypixelart = PyPixelArt(image, path)
+    pypixelart.run_loop()
 
 
 if __name__ == "__main__":
